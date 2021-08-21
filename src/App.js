4d9f36db397838218334de3048f37ed8a1f1619d
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-08-21 09:23:31 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import backgrounVid from './assets/videos/backgroundVid.mp4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      <video autoPlay loop muted>
+        <source src={backgrounVid} type='video/mp4' />
+      </video>
     </div>
   );
-}
+};
 
 export default App;
