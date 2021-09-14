a405f9dce0c3814b65f6247b218be1272f6a0c56
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-09-13 23:17:38 -0700

    project cards layout

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 import useStyles from './styles';
 
@@ -20,6 +21,24 @@ const Projects = () => {
                     Portfolio
                 </Typography>
             </Grid>
+            
+                <Paper style={{width: '100%', padding: '50px 30px'}}>
+                    <Grid container justifyContent='center'>
+                    <div style={{margin: '20px 25px', height: '400px', width: '250px', background: 'rgba(255,255,255,.05)', borderLeft: 'solid 1px rgba(255,255,255,.5)', borderTop: 'solid 1px rgba(255,255,255,.5)', borderRadius: '5px', boxShadow: '20px 20px 50px rgba(0,0,0,.5)'}}>
+                        HELLO
+                    </div>
+                    <div style={{margin: '20px 25px', height: '400px', width: '250px', background: 'rgba(255,255,255,.05)', borderLeft: 'solid 1px rgba(255,255,255,.5)', borderTop: 'solid 1px rgba(255,255,255,.5)', borderRadius: '5px', boxShadow: '20px 20px 50px rgba(0,0,0,.5)'}}>
+                        HELLO
+                    </div>
+                    <div style={{margin: '20px 25px', height: '400px', width: '250px', background: 'rgba(255,255,255,.05)', borderLeft: 'solid 1px rgba(255,255,255,.5)', borderTop: 'solid 1px rgba(255,255,255,.5)', borderRadius: '5px', boxShadow: '20px 20px 50px rgba(0,0,0,.5)'}}>
+                        HELLO
+                    </div>
+                    <div style={{margin: '20px 25px', height: '400px', width: '250px', background: 'rgba(255,255,255,.05)', borderLeft: 'solid 1px rgba(255,255,255,.5)', borderTop: 'solid 1px rgba(255,255,255,.5)', borderRadius: '5px', boxShadow: '20px 20px 50px rgba(0,0,0,.5)'}}>
+                        HELLO
+                    </div>
+                    </Grid>
+                </Paper>
+            
         </>
     )
 }
